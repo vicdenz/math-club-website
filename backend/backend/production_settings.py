4624b9c58d24d557cd24868ab5b5aba57faa8dcf
vicdenz <david.daniliuc24@gmail.com>
2024-03-15 22:07:21 -0400

    Update production_settings.py

--- backend/backend/production_settings.py
+++ backend/backend/production_settings.py
@@ -5,6 +5,7 @@ when you are in production environment.
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

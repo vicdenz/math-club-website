b62dbf54c33171f096b48fe844d7396e2c2ef9e0
vicdenz <david.daniliuc24@gmail.com>
2024-03-18 08:55:00 -0400

    Debug PROD ENv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,10 @@
 import os
 
-if os.environ.get("DJANGO_ENV") == "PRODUCTION":
-    from .production_settings import *
+print("Debug:", os.environ)
+current_env = os.environ.get("DJANGO_ENV")
+if current_env == "PRODUCTION":
+	print("PRODUCTION SETTINGS")
+	from .production_settings import *
 else:
-    from .local_settings import *
+	print("LOCAL SETTINGS")
+	from .local_settings import *
